1a6924fc385314531ff2e52717c253f928d361d1
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-10-19 18:00:08 -0300

    Criando a seleção e a conclusão de tarefas no projeto

--- script-crud.js
+++ script-crud.js
@@ -5,13 +5,41 @@ const formLabel = document.querySelector('.app__form-label');
 const textarea = document.querySelector('.app__form-textarea');
 const cancelFormTaskBtn = document.querySelector('.app__form-footer__button--cancel');
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
+const localStorageTasks = localStorage.getItem('tasks');
+const taskActiveDescription = document.querySelector('.app__section-active-task-description');
 
-let tasks = [];
+let tasks = localStorageTasks ? JSON.parse(localStorageTasks) : [];
+let taskSelected = null;
+let itemTaskSelected = null;
+
+const taskSelect = (task, element) => {
+    document.querySelectorAll('.app__section-task-list-item-active').forEach(function (button) {
+        button.classList.remove('app__section-task-list-item-active');
+    });
+
+    if (taskSelect == task) {
+        taskActiveDescription.textContent = null;
+        itemTaskSelected = null;
+        taskSelected = null;
+        return;
+    }
+    
+    taskSelected = task;
+    itemTaskSelected = element;
+    taskActiveDescription.textContent = task.descricao;
+    element.classList.add('app__section-task-list-item-active');
+}
 
 const clearForm = () => {
     textarea.value = '';
     formTask.classList.add('hidden');
 }
+
+cancelFormTaskBtn.addEventListener('click', () => {
+    formTask.classList.add('hidden');
+});
+
+btnCancelar.addEventListener('click', clearForm);
 
 const taskIconSvg = `
 <svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -28,6 +56,19 @@ function createTask(task) {
     const paragraph = document.createElement('p');
     paragraph.classList.add('app__section-task-list-item-description');
     paragraph.textContent = task.descricao;
+    const button = document.createElement('button');
+    li.onclick = () => {
+        taskSelect(task, li);
+    }
+    svgIcon.addEventListener('click', (event) => {
+        event.stopPropagation();
+        button.setAttribute('disabled', true);
+        li.classList.add('app__section-task-list-item-complete');
+    });
+    if (task.concluida) {
+        button.setAttribute('disabled', true);
+        li.classList.add('app__section-task-list-item-complete');
+    }
     li.appendChild(svgIcon);
     li.appendChild(paragraph);
     return li;
@@ -43,6 +84,10 @@ toggleFormTaskBtn.addEventListener('click', () => {
     formTask.classList.toggle('hidden');
 });
 
+const updateLocalStorage = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 formTask.addEventListener('submit', (evento) => {
     evento.preventDefault();
     const task = {
@@ -52,11 +97,6 @@ formTask.addEventListener('submit', (evento) => {
     tasks.push(task);
     const taskItem = createTask(task);
     taskListContainer.appendChild(taskItem);
+    updateLocalStorage();
     clearForm();
 });
-
-cancelFormTaskBtn.addEventListener('click', () => {
-    formTask.classList.add('hidden');
-});
-
-btnCancelar.addEventListener('click', clearForm);
